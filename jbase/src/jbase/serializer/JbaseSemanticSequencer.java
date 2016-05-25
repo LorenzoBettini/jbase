0c713d6bc6006ed80dfff9865fe553c2f100979e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-05-25 16:22:18 +0200

    27: ported serializer
    Task-Url: https://github.com/LorenzoBettini/jbase/issues/issue/27

--- jbase/src/jbase/serializer/JbaseSemanticSequencer.java
+++ jbase/src/jbase/serializer/JbaseSemanticSequencer.java
@@ -5,7 +5,7 @@ package jbase.serializer;
 
 import java.util.List;
 
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.serializer.ISerializationContext;
 import org.eclipse.xtext.serializer.acceptor.SequenceFeeder;
 import org.eclipse.xtext.serializer.sequencer.ISemanticNodeProvider.INodesForEObjectProvider;
 import org.eclipse.xtext.xbase.XExpression;
@@ -32,32 +32,32 @@ public class JbaseSemanticSequencer extends XbaseSemanticSequencerAccess {
 	@Inject
 	private JbaseModelUtil modelUtil;
 
-//	@Override
-//	protected void sequence_XJArrayConstructorCall(EObject context, XJArrayConstructorCall semanticObject) {
-//		INodesForEObjectProvider nodes = createNodeProvider(semanticObject);
-//		SequenceFeeder acceptor = createSequencerFeeder(semanticObject, nodes);
-//		XJArrayConstructorCallElements elements = access.getXJArrayConstructorCallAccess();
-//
-//		acceptor.accept(elements.getTypeJvmTypeQualifiedNameParserRuleCall_0_0_2_0_1(), semanticObject.getType());
-//
-//		List<XExpression> associations = modelUtil.arrayDimensionIndexAssociations(semanticObject);
-//		List<XJArrayDimension> dimensions = semanticObject.getDimensions();
-//		acceptor.accept(elements.getDimensionsXJArrayDimensionParserRuleCall_0_0_3_0(), dimensions.get(0), 0);
-//		if (associations.get(0) != null) {
-//			acceptor.accept(elements.getIndexesXExpressionParserRuleCall_1_0(), semanticObject.getIndexes().get(0), 0);
-//		}
-//		for (int i = 1; i < associations.size(); i++) {
-//			acceptor.accept(elements.getDimensionsXJArrayDimensionParserRuleCall_3_0_0(), dimensions.get(i), i);
-//			if (associations.get(i) != null) {
-//				acceptor.accept(elements.getIndexesXExpressionParserRuleCall_3_1_0(), associations.get(i), i);
-//			}
-//		}
-//
-//		if (semanticObject.getArrayLiteral() != null) {
-//			acceptor.accept(elements.getArrayLiteralXJArrayLiteralParserRuleCall_4_0(),
-//					semanticObject.getArrayLiteral());
-//		}
-//
-//		acceptor.finish();
-//	}
+	@Override
+	protected void sequence_XJArrayConstructorCall(ISerializationContext context, XJArrayConstructorCall semanticObject) {
+		INodesForEObjectProvider nodes = createNodeProvider(semanticObject);
+		SequenceFeeder acceptor = createSequencerFeeder(context, semanticObject, nodes);
+		XJArrayConstructorCallElements elements = access.getXJArrayConstructorCallAccess();
+
+		acceptor.accept(elements.getTypeJvmTypeQualifiedNameParserRuleCall_0_0_2_0_1(), semanticObject.getType());
+
+		List<XExpression> associations = modelUtil.arrayDimensionIndexAssociations(semanticObject);
+		List<XJArrayDimension> dimensions = semanticObject.getDimensions();
+		acceptor.accept(elements.getDimensionsXJArrayDimensionParserRuleCall_0_0_3_0(), dimensions.get(0), 0);
+		if (associations.get(0) != null) {
+			acceptor.accept(elements.getIndexesXExpressionParserRuleCall_1_0(), semanticObject.getIndexes().get(0), 0);
+		}
+		for (int i = 1; i < associations.size(); i++) {
+			acceptor.accept(elements.getDimensionsXJArrayDimensionParserRuleCall_3_0_0(), dimensions.get(i), i);
+			if (associations.get(i) != null) {
+				acceptor.accept(elements.getIndexesXExpressionParserRuleCall_3_1_0(), associations.get(i), i);
+			}
+		}
+
+		if (semanticObject.getArrayLiteral() != null) {
+			acceptor.accept(elements.getArrayLiteralXJArrayLiteralParserRuleCall_4_0(),
+					semanticObject.getArrayLiteral());
+		}
+
+		acceptor.finish();
+	}
 }
