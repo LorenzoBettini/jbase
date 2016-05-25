a914862a9973f14f7ac7995c83933696893802a3
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-05-25 11:33:00 +0200

    27: updated custom injector provider
    Task-Url: https://github.com/LorenzoBettini/jbase/issues/issue/27

--- jbase.tests/src/jbase/tests/util/SimpleJvmModelTestInjectorProvider.java
+++ jbase.tests/src/jbase/tests/util/SimpleJvmModelTestInjectorProvider.java
@@ -2,29 +2,19 @@ package jbase.tests.util;
 
 import org.eclipse.xtext.xbase.jvmmodel.IJvmModelInferrer;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-
-import jbase.JbaseRuntimeModule;
-import jbase.JbaseStandaloneSetup;
 import jbase.JbaseInjectorProvider;
+import jbase.JbaseRuntimeModule;
 
 public class SimpleJvmModelTestInjectorProvider extends JbaseInjectorProvider {
+
 	@Override
-	protected Injector internalCreateInjector() {
-		return new SimpleJvmModelTestStandaloneSetup().createInjectorAndDoEMFRegistration();
+	protected JbaseRuntimeModule createRuntimeModule() {
+		return new JbaseRuntimeModule() {
+			@Override
+			public Class<? extends IJvmModelInferrer> bindIJvmModelInferrer() {
+				return SimpleJvmModelInferrer.class;
+			}
+		};
 	}
 
-	public static class SimpleJvmModelTestStandaloneSetup extends JbaseStandaloneSetup {
-		@Override
-		public Injector createInjector() {
-			return Guice.createInjector(new JbaseRuntimeModule() {
-				@Override
-				public Class<? extends IJvmModelInferrer> bindIJvmModelInferrer() {
-					return SimpleJvmModelInferrer.class;
-				}
-			});
-		}
-	}
-	
 }
