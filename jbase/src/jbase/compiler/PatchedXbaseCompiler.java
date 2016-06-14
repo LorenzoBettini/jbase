d6791aecda7e2ee774217cab48dc4288e657a104
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-06-14 12:40:37 +0200

    42: removed custom patched compiler method
    Temporary fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=489037
    
    Task-Url: https://github.com/LorenzoBettini/jbase/issues/42

--- jbase/src/jbase/compiler/PatchedXbaseCompiler.java
+++ jbase/src/jbase/compiler/PatchedXbaseCompiler.java
@@ -1,28 +1,13 @@
 package jbase.compiler;
 
-import org.eclipse.xtext.xbase.XAssignment;
-import org.eclipse.xtext.xbase.XExpression;
 import org.eclipse.xtext.xbase.compiler.XbaseCompiler;
-import org.eclipse.xtext.xbase.compiler.output.ITreeAppendable;
 
 /**
- * Temporary fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=489037
+ * For temporary fixes.
  * 
  * @author Lorenzo Bettini
  *
  */
 public class PatchedXbaseCompiler extends XbaseCompiler {
 
-	@Override
-	protected boolean isVariableDeclarationRequired(XExpression expr, ITreeAppendable b) {
-		if (expr instanceof XAssignment) {
-			XAssignment a = (XAssignment) expr;
-			for (XExpression arg : getActualArguments(a)) {
-				if (isVariableDeclarationRequired(arg, b)) {
-					return true;
-				}
-			}
-		}
-		return super.isVariableDeclarationRequired(expr, b);
-	}
 }
