0aa748fa48e58c6461e38be873df127e0b634c94
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2017-07-17 12:24:58 +0200

    -Dtycho.disableP2Mirrors=true

--- travis_build.sh
+++ travis_build.sh
@@ -4,11 +4,11 @@ set -ev
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
 		echo "Build on MacOSX: Pull Request"
-		mvn -f jbase.releng/pom.xml clean verify
+		mvn -f jbase.releng/pom.xml clean verify -Dtycho.disableP2Mirrors=true
 	else
 		echo "Skipping build on MacOSX for standard commit"
 	fi
 else
 	echo "Build on Linux"
-	mvn -f jbase.releng/pom.xml clean verify -Pjacoco-report coveralls:report
+	mvn -f jbase.releng/pom.xml clean verify -Pjacoco-report coveralls:report -Dtycho.disableP2Mirrors=true
 fi 
