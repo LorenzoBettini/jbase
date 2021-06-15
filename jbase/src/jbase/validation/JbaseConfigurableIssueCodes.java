da9745b537e411aba2a2855b40e288a58a50393f
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-15 18:17:22 +0200

    fixed access to COPY_JAVA_PROBLEMS

--- jbase/src/jbase/validation/JbaseConfigurableIssueCodes.java
+++ jbase/src/jbase/validation/JbaseConfigurableIssueCodes.java
@@ -2,6 +2,7 @@ package jbase.validation;
 
 import org.eclipse.xtext.preferences.PreferenceKey;
 import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.validation.IssueCodes;
 import org.eclipse.xtext.validation.SeverityConverter;
 import org.eclipse.xtext.xbase.validation.XbaseConfigurableIssueCodes;
 
@@ -17,7 +18,7 @@ public class JbaseConfigurableIssueCodes extends XbaseConfigurableIssueCodes {
 	protected void initialize(IAcceptor<PreferenceKey> iAcceptor) {
 		super.initialize(iAcceptor);
 		// overwrite xbase default
-		iAcceptor.accept(create(org.eclipse.xtext.xbase.validation.IssueCodes.COPY_JAVA_PROBLEMS,
+		iAcceptor.accept(create(IssueCodes.COPY_JAVA_PROBLEMS,
 				SeverityConverter.SEVERITY_ERROR));
 	}
 }
