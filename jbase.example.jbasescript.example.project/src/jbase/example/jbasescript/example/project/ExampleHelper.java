45137a233d8008831bb9d63849c73ba02deb180e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-06-18 16:01:09 +0200

    114: added Helper Java class
    Task-Url: https://github.com/LorenzoBettini/jbase/issues/114

---
+++ jbase.example.jbasescript.example.project/src/jbase/example/jbasescript/example/project/ExampleHelper.java
@@ -0,0 +1,12 @@
+package jbase.example.jbasescript.example.project;
+
+public class ExampleHelper {
+
+	public String aFile() {
+		return "File: ";
+	}
+
+	public String aDir() {
+		return "Directory: ";
+	}
+}
