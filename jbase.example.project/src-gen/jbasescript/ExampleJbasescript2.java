7b744fe310242e27a712693760388ba51656f0bc
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2021-10-18 18:45:04 +0200

    131: Additional examples to recreate the problem manually
    Task-Url: https://github.com/LorenzoBettini/jbase/issues/131

---
+++ jbase.example.project/src-gen/jbasescript/ExampleJbasescript2.java
@@ -0,0 +1,8 @@
+package jbasescript;
+
+@SuppressWarnings("all")
+public class ExampleJbasescript2 {
+  public static void main(String[] args) throws Throwable {
+    System.out.println("hello");
+  }
+}
---
+++ jbase.example.project/src-gen/jbasetestlanguage/ExampleJbasetestlanguage2.java
@@ -0,0 +1,8 @@
+package jbasetestlanguage;
+
+@SuppressWarnings("all")
+public class ExampleJbasetestlanguage2 {
+  public static void main(String[] args) throws Throwable {
+    System.out.println("he");
+  }
+}
